17a287a7d7f065a26cc80b9c00ffc4e15c5865f6
Dam Hong Duc <hongduc001h@gmail.com>
2022-12-14 09:31:06 +0700

    set up eslint, prettier, lint-staged, husky

---
+++ src/SplashPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+//const a = 5;
+
+export default function SplashPage() {
+  return (
+    <View>
+      <Text>hesslloggfsg</Text>
+    </View>
+  );
+}
