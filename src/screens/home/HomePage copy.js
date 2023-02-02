8bc1406d3d60bb4fa8733c6c4bff95863a49ceef
Dam Hong Duc <hongduc001h@gmail.com>
2023-02-02 16:24:34 +0700

    test pre commit

---
+++ src/screens/home/HomePage copy.js
@@ -0,0 +1,16 @@
+import { StyleSheet, SafeAreaView } from "react-native";
+import TopTabStack from "navigation/TopTabStack";
+
+export default function HomePage() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <TopTabStack></TopTabStack>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
