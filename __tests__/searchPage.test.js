a622047f5e10ce6b8b73a155df5b8c27025e7120
Dam Hong Duc <hongduc001h@gmail.com>
2023-02-09 15:07:41 +0700

    wip

--- __tests__/searchPage.test.js
+++ __tests__/searchPage.test.js
@@ -1,50 +1,13 @@
-// import SearchPage from "screens/search/searchPage";
-// import { render, fireEvent } from "@testing-library/react-native";
-
-// const mockDispatch = jest.fn();
-// const mockSelector = jest.fn();
-
-// jest.mock("react-redux", () => ({
-//   ...jest.requireActual("react-redux"),
-//   useDispatch: () => mockDispatch,
-//   useSelector: () => mockSelector,
-// }));
-// jest.mock("react-native/Libraries/EventEmitter/NativeEventEmitter");
-
-// jest.mock("react-native-track-player", () => {
-//   return {
-//     usePlaybackState: jest.fn(),
-//     State: jest.fn(),
-//   };
-// });
-
-// test("test search page", () => {
-//   const navigation = jest.fn();
-
-//   const { getByTestId } = render(<SearchPage navigation={navigation} />);
-
-//   const searchButton = getByTestId("searchButton");
-
-//   fireEvent.changeText(getByTestId("searchText"), "content=searchText");
-//   fireEvent.press(searchButton);
-// });
-
-import Example from "screens/search/Example";
 import { render, screen, fireEvent } from "@testing-library/react-native";
+import SearchPage from "screens/search/searchPage";
 
-test("examples of some things", async () => {
-  const expectedUsername = "Ada Lovelace";
+const navigation = jest.fn();
 
-  render(<Example />);
+test("test search page", async () => {
+  const expectSearchText = "Son Tung";
 
-  fireEvent.changeText(screen.getByTestId("input"), expectedUsername);
-  fireEvent.press(screen.getByText("Print Username"));
+  render(<SearchPage navigation={navigation} />);
 
-  // Using `findBy` query to wait for asynchronous operation to finish
-  const usernameOutput = await screen.findByTestId("printed-username");
-
-  // Using `toHaveTextContent` matcher from `@testing-library/jest-native` package.
-  expect(usernameOutput).toHaveTextContent(expectedUsername);
-
-  expect(screen.toJSON()).toMatchSnapshot();
+  fireEvent.changeText(screen.getByTestId("searchText"), expectSearchText);
+  fireEvent.press(screen.getByTestId("searchButton"));
 });
