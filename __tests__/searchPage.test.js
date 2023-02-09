f83ce54c3cf40b1763f40609c225508071db4345
Dam Hong Duc <hongduc001h@gmail.com>
2023-02-09 16:34:08 +0700

    add some mock

--- __tests__/searchPage.test.js
+++ __tests__/searchPage.test.js
@@ -1,7 +1,23 @@
 import { render, screen, fireEvent } from "@testing-library/react-native";
 import SearchPage from "screens/search/searchPage";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import { createSlice } from "@reduxjs/toolkit";
+
+jest.mock("axios");
+import axios from "axios";
 
 const navigation = jest.fn();
+
+beforeAll(() => {
+  axios.get.mockImplementation(() => Promise.resolve("whatever-get"));
+  axios.post.mockImplementation(() => Promise.resolve("whatever-post"));
+});
+
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useSelector: jest.fn(),
+}));
 
 test("test search page", async () => {
   const expectSearchText = "Son Tung";
--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,8 @@ module.exports = {
       "|react-native-vector-icons" +
       "|react-native-popover-view" +
       "|@react-native-firebase" +
+      "|axios" +
+      "|react-redux" +
       ")/)",
   ],
 };
