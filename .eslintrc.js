94fe69fa6927d192ea8359e9433b19516b9ff3fc
Dam Hong Duc <hongduc001h@gmail.com>
2023-06-24 16:56:43 +0700

    feat: add typescript

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: false,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+};
--- index.js
+++ index.js
@@ -2,14 +2,13 @@
  * @format
  */
 
-import "./shim";
-import { AppRegistry } from "react-native";
-import App from "./App";
-import { name as appName } from "./app.json";
-import { PlaybackService } from "services/PlaybackService";
-import TrackPlayer from "react-native-track-player";
+import './shim';
+import { AppRegistry } from 'react-native';
+import App from './src/App';
+import { name as appName } from './app.json';
+import { PlaybackService } from 'services/PlaybackService';
+import TrackPlayer from 'react-native-track-player';
 
 AppRegistry.registerComponent(appName, () => App);
 
 TrackPlayer.registerPlaybackService(() => PlaybackService);
-
